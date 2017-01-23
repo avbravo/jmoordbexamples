5e700c2ff128c201480b5734829b21675792f565
avbravo <avbravo@avbravo>
2017-01-23 16:01:38 -0500

    create,delete, listcollections, isExist()

--- src/main/java/com/avbravo/jmoordbexamples/ejb/AutoincrementableFacade.java
+++ src/main/java/com/avbravo/jmoordbexamples/ejb/AutoincrementableFacade.java
@@ -5,34 +5,44 @@
  */
 package com.avbravo.jmoordbexamples.ejb;
 
-
 import com.avbravo.jmoordb.facade.AbstractFacade;
 import com.avbravo.jmoordbexamples.entity.Autoincrementable;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
 
 /**
  *
  * @author avbravo
  */
-public class AutoincrementableFacade extends AbstractFacade<Autoincrementable>{
+public class AutoincrementableFacade extends AbstractFacade<Autoincrementable> {
 
-    public AutoincrementableFacade( ){
+    public AutoincrementableFacade() {
         super(Autoincrementable.class, "fantasy", "autoincrementable");
     }
 
     @Override
     protected MongoClient getMongoClient() {
-         MongoClient mongoClient = new MongoClient();
-         return mongoClient;
+        MongoClient mongoClient = new MongoClient();
+        return mongoClient;
     }
- @Override
+
+    protected MongoClient getMongoClient(String database, String username, String password) {
+        char[] charArray = password.toCharArray();
+        MongoCredential credential = MongoCredential.createCredential(username, database, charArray);
+        MongoClient mongoClient = new MongoClient(new ServerAddress("localhost",27017), Arrays.asList(credential));
+        return mongoClient;
+    }
+
+    @Override
     public Object findById(String key, String value) {
-       return find(key,value);
+        return find(key, value);
     }
 
     @Override
     public Object findById(String key, Integer value) {
-        return find(key,value);
+        return find(key, value);
     }
 }
